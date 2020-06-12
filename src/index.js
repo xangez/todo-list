49e6c27f1bf13ba2086ff57dd714d419cfb0952b
xangez <xangez348@gmail.com>
2020-06-12 22:05:40 +1000

    basic js added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,48 @@
 // import contacts from './contacts';
 
 (function(){
-  console.log('hi')
+
+  const mainContainer = document.querySelector('main-container');
+
+  //mylists
+  const toggleListsContainer = document.querySelector('#togglelists-container');
+  const listContainerTemplate = document.querySelector('#listContainer-template');
+
+
+  //todo form
+  const addTodoForm = document.querySelector('.addTodo-form');
+  addTodoForm.addEventListener('submit', addTodo);
+
+  //openlist
+  const openListName = document.querySelector('.openList-name');
+  const openListContainer = document.querySelector('openList-container');
+
+  function addTodo(e) {
+    e.preventDefault();
+    openListName.textContent = 'hi';
+  }
+
+  
+
+  function createList(name) {
+    return {name: name, tasks:[]}
+  } 
+
+  function createTodoItem(info) {
+    return {info: info};
+  }
+
+  
+
+  function renderOpenList() {
+    mainContainer.lastChild.remove();
+    const openListTemplate = document.querySelector('openList-template');
+
+
+
+  }
+
+
 
 })();
 
